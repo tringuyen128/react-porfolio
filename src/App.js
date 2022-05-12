48f84cc20da95ad16cccffa0b43f22e817109df8
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-05-11 19:21:41 -0700

    contact

--- src/App.js
+++ src/App.js
@@ -25,5 +25,4 @@ const App = () => {
     </HashRouter>
   )
 }
-
 export default App
---
+++ src/components/Contact/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdbreact'
+
+const ContactInfo = () => {
+  return (
+    <MDBContainer className="mt-5">
+      <MDBRow>
+        <MDBCol md="12" className="text-center">
+          <h2>Contact Information</h2>
+          <hr class="solid bg-dark" />
+
+          <h4>
+            I recently gradated a Full Stack Web Development Bootcamp at the UC
+            Berkeley Extension. I am actively looking for a Junior Web
+            Development position, below I have linked my e-mail and cell phone
+            number. Please feel free to contact me at any time and if I do not
+            immediately respond I will respond within the next buisness day.
+            Thank you!
+          </h4>
+          <hr className="hr-light w-25" />
+          <h3>Cell Phone</h3>
+          <hr className="hr-dark w-25" />
+          <a href="tel:801-792-5502">
+            <MDBIcon icon="phone-alt" /> 408-627-5078
+          </a>
+          <hr className="hr-light w-25" />
+          <h3>E-mail</h3>
+          <hr className="hr-dark w-25" />
+          <a href="mailto:collinhodgs@gmail.com">
+            <MDBIcon icon="envelope" /> mr.tringuyen1225@gmail.com
+          </a>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  )
+}
+
+export default ContactInfo
