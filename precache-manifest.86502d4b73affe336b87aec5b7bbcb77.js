27f3b7c5c8064bb5f11362de2f7164f939a589ae
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-05-13 11:27:59 -0700

    Updates

---
+++ precache-manifest.86502d4b73affe336b87aec5b7bbcb77.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d2f893732dd6cbfdc8268eb7e99f5fe",
+    "url": "/react-porfolio/index.html"
+  },
+  {
+    "revision": "e7c445395fe5247ea11b",
+    "url": "/react-porfolio/static/css/2.709cd8e7.chunk.css"
+  },
+  {
+    "revision": "3b75a16eff8db19d215b",
+    "url": "/react-porfolio/static/css/main.1250a84e.chunk.css"
+  },
+  {
+    "revision": "e7c445395fe5247ea11b",
+    "url": "/react-porfolio/static/js/2.05a92d3e.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/react-porfolio/static/js/2.05a92d3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b75a16eff8db19d215b",
+    "url": "/react-porfolio/static/js/main.96bb40a5.chunk.js"
+  },
+  {
+    "revision": "9096492c29e4fbf07aab",
+    "url": "/react-porfolio/static/js/runtime-main.1632420f.js"
+  },
+  {
+    "revision": "2012387807f50b7d43915f3605cc4e7a",
+    "url": "/react-porfolio/static/media/Tri Nguyen.20123878.pdf"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/react-porfolio/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/react-porfolio/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "19a60a64ff83c7972e9939db8e85e5be",
+    "url": "/react-porfolio/static/media/budget.19a60a64.jpg"
+  },
+  {
+    "revision": "2f12242375edd68e9013ecfb59c672e9",
+    "url": "/react-porfolio/static/media/fa-brands-400.2f122423.svg"
+  },
+  {
+    "revision": "8300bd7f30e0a313c1d772b49d96cb8e",
+    "url": "/react-porfolio/static/media/fa-brands-400.8300bd7f.ttf"
+  },
+  {
+    "revision": "ad527cc5ec23d6da66e8a1d6772ea6d3",
+    "url": "/react-porfolio/static/media/fa-brands-400.ad527cc5.woff"
+  },
+  {
+    "revision": "e2ca6541bff3a3e9f4799ee327b28c58",
+    "url": "/react-porfolio/static/media/fa-brands-400.e2ca6541.eot"
+  },
+  {
+    "revision": "f075c50f89795e4cdb4d45b51f1a6800",
+    "url": "/react-porfolio/static/media/fa-brands-400.f075c50f.woff2"
+  },
+  {
+    "revision": "3602b7e8b2cb1462b0bef9738757ef8a",
+    "url": "/react-porfolio/static/media/fa-regular-400.3602b7e8.svg"
+  },
+  {
+    "revision": "3c6879c4f342203d099bdd66dce6d396",
+    "url": "/react-porfolio/static/media/fa-regular-400.3c6879c4.woff"
+  },
+  {
+    "revision": "49f00693b0e5d45097832ef5ea1bc541",
+    "url": "/react-porfolio/static/media/fa-regular-400.49f00693.ttf"
+  },
+  {
+    "revision": "4a74738e7728e93c4394b8604081da62",
+    "url": "/react-porfolio/static/media/fa-regular-400.4a74738e.woff2"
+  },
+  {
+    "revision": "b01516c1808be557667befec76cd6318",
+    "url": "/react-porfolio/static/media/fa-regular-400.b01516c1.eot"
+  },
+  {
+    "revision": "205f07b3883c484f27f40d21a92950d4",
+    "url": "/react-porfolio/static/media/fa-solid-900.205f07b3.ttf"
+  },
+  {
+    "revision": "4451e1d86df7491dd874f2c41eee1053",
+    "url": "/react-porfolio/static/media/fa-solid-900.4451e1d8.woff"
+  },
+  {
+    "revision": "664de3932dd6291b4b8a8c0ddbcb4c61",
+    "url": "/react-porfolio/static/media/fa-solid-900.664de393.svg"
+  },
+  {
+    "revision": "8ac3167427b1d5d2967646bd8f7a0587",
+    "url": "/react-porfolio/static/media/fa-solid-900.8ac31674.eot"
+  },
+  {
+    "revision": "8e1ed89b6ccb8ce41faf5cb672677105",
+    "url": "/react-porfolio/static/media/fa-solid-900.8e1ed89b.woff2"
+  },
+  {
+    "revision": "07383cd58b1edf9f4a9482a52f0865dd",
+    "url": "/react-porfolio/static/media/netflix.07383cd5.png"
+  },
+  {
+    "revision": "857c584f3fa33a100a9986dfbe252767",
+    "url": "/react-porfolio/static/media/news.857c584f.jpeg"
+  },
+  {
+    "revision": "06439080719d6342119a78ef2e5a91d6",
+    "url": "/react-porfolio/static/media/notetaking.06439080.png"
+  },
+  {
+    "revision": "2117ad3000d32fb580067127d508c078",
+    "url": "/react-porfolio/static/media/weather.2117ad30.png"
+  },
+  {
+    "revision": "0edfcbe5d0b1043064e820c74f4d1887",
+    "url": "/react-porfolio/static/media/workdayschedule.0edfcbe5.png"
+  }
+]);
